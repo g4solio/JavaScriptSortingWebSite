e17a9ab532c0f3be7befe8a2e89f0bdd4687f501
g4solio <daddidaffi@gmail.com>
2017-03-30 10:13:39 +0200

    aggiornato script graph

--- javascript/Main.js
+++ javascript/Main.js
@@ -2,29 +2,17 @@
  * Created by daddi on 3/25/2017.
  */
 var myCanvas = document.getElementById("myChart");
-var array = [12, 19, 3, 5, 2, 3,7,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var array = [];
 var myChart = new Chart(myCanvas, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: [],
         datasets: [{
             label: '# of Votes',
             data: array,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
@@ -37,8 +25,9 @@ var myChart = new Chart(myCanvas, {
                 }
             }],
             xAxes: [{
-                categoryPercentage: 1,
-                barPercentage:1
+				stacked:true,
+				categoryPercentage: 1,
+				barPercentage: 1.0
             }]
         }
     }
@@ -67,6 +56,12 @@ buttonShuffle.addEventListener("click",function()
 {
     myChart.clear();
     setTimeout(myChart.data.datasets[0].data = shuffle(myChart.data.datasets[0].data), 2000);
+	var value = Math.random();
+	myChart.data.datasets[0].data[myChart.data.datasets[0].data.length] = value;
+	myChart.data.labels[myChart.data.datasets[0].data.length-1] = "" + value;
+	myChart.data.datasets[0].backgroundColor[myChart.data.datasets[0].data.length-1] = 'rgba(255,99,132,1)';
+	myChart.data.datasets[0].borderColor[myChart.data.datasets[0].data.length-1] = 'rgba(255,99,132,1)';
+	
     myChart.update();
 },false);
 
