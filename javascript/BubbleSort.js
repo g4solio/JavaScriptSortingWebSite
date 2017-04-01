7be076e0de7a68d5820c58a210705ff5211e2d2f
g4solio <daddidaffi@gmail.com>
2017-04-01 11:02:19 +0200

    cose

--- javascript/BubbleSort.js
+++ javascript/BubbleSort.js
@@ -1,30 +1,34 @@
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
+function sleep(miliseconds) {
+   var currentTime = new Date().getTime();
+
+   while (currentTime + miliseconds >= new Date().getTime()) {
+   }
 }
 function BubbleSort(array)
 {
+	
     var swapped;
     do {
         swapped = false;
         for (var i=0; i < array.length-1; i++) {
-			myChart.clear();
-			myChart.data.datasets[0].backgroundColor[i] = 'rgba(100,100,132,1)';
+			//myChart.data.datasets[0].backgroundColor[i] = 'rgba(100,100,132,1)';
+			//myChart.data.datasets[0].backgroundColor[i+1] = 'rgba(1,1,1,1)';
             if (array[i] > array[i+1]) {
                 var temp = array[i];
                 array[i] = array[i+1];
                 array[i+1] = temp;
                 swapped = true;
             }
-			myChart.update(10000,false);
-			alert("sono qui");
+			
+			//myChart.render(1,false);
+			
+			//alert("sono qui");
 			//myChart.render();
-			myChart.data.datasets[0].backgroundColor[i] = 'rgba(255,99,132,1)';
+			//myChart.data.datasets[0].backgroundColor[i] = 'rgba(255,99,132,1)';
+			//myChart.data.datasets[0].backgroundColor[i+1] = 'rgba(255,99,132,1)';
+
         }
+
     } while (swapped);
 }
  
--- javascript/ChartInstance.js
+++ javascript/ChartInstance.js
@@ -1,7 +1,7 @@
 /**
  * Created by daddi on 3/25/2017.
  */
-var myCanvas = document.getElementById("myChart");
+var myCanvas = document.getElementById("myChart").getContext("2dw");
 var array = [];
 var myChart = new Chart(myCanvas, {
     type: 'bar',
